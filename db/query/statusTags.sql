95cfc4ec67a1344f6c2a892251d1c32e47325462
sikigasa <km3t1114@yahoo.co.jp>
2023-07-10 16:47:17 +0900

    add: C＆R_hackathons_status_tags

--- db/query/statusTags.sql
+++ db/query/statusTags.sql
@@ -4,6 +4,16 @@ FROM status_tags
 where hackathon_id = $1;
 
 --name CreateHackathonStatusTag :one
-
+INSERT INTO hackathon_status_tags (
+    hackathon_id,
+    status_id
+  )VALUES(
+    $1,
+    $2
+  )
+RETURNING *;
 
 --name GetHackathonStatusTag :one
+SELECT *
+FROM hackathon_status_tags
+WHERE hackathon_id = $1;
