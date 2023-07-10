39a9ca74eefbbf24443915634e03a5de60636f92
sikigasa <km3t1114@yahoo.co.jp>
2023-07-10 19:09:26 +0900

    fix:TestListStatusTags

--- db/sqlc/statusTags_test.go
+++ db/sqlc/statusTags_test.go
@@ -9,14 +9,10 @@ import (
 
 var statusData = []StatusTags{
 	{1, "オンライン"},
-	{2, "オフライン"},
-	{3, "初心者歓迎"},
-	{4, "急募"},
 }
 
 func TestListStatusTags(t *testing.T) {
-
-	statusTags, err := testQueries.ListStatusTags(context.Background(), 1)
+	statusTags, err := testQueries.ListStatusTags(context.Background(), int32(1))
 	require.NoError(t, err)
 	require.NotEmpty(t, statusTags)
 
