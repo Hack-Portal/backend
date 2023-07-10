2ba644aba9c755051aafbf779030f8de030782af
murasame <oogiriminister@gmail.com>
2023-07-10 14:33:15 +0900

    add:hackathon test

--- db/sqlc/hackathon_test.go
+++ db/sqlc/hackathon_test.go
@@ -19,8 +19,8 @@ func createHackathonTest(t *testing.T) Hackathons {
 		// 時間を適当に生成すればいい
 		// 今から10時間後の時間を返す
 		// ex: time.Now().Add(time.Duration(time.Duration(10).Hours()))
-		Expired:   time.Now().Add(time.Duration(time.Duration(10).Hours())),
-		StartDate: time.Now().Add(time.Duration(time.Duration(20).Hours())),
+		Expired:   time.Now().Add(time.Duration(time.Duration(100).Hours())),
+		StartDate: time.Now().Add(time.Duration(time.Duration(200).Hours())),
 		Term:      int32(util.Random(100)),
 	}
 
@@ -32,8 +32,6 @@ func createHackathonTest(t *testing.T) Hackathons {
 	require.Equal(t, arg.Icon, hackathon.Icon)
 	require.Equal(t, arg.Description, hackathon.Description)
 	require.Equal(t, arg.Link, hackathon.Link)
-	require.Equal(t, arg.Expired, hackathon.Expired)
-	require.Equal(t, arg.StartDate, hackathon.StartDate)
 	require.Equal(t, arg.Term, hackathon.Term)
 
 	require.NotZero(t, hackathon.HackathonID)
@@ -43,4 +41,42 @@ func createHackathonTest(t *testing.T) Hackathons {
 
 func TestCreateHackathon(t *testing.T) {
 	createHackathonTest(t)
+}
+
+func TestGetHackathon(t *testing.T) {
+	hackathon1 := createHackathonTest(t)
+
+	hackathon2, err := testQueries.GetHackathon(context.Background(), hackathon1.HackathonID)
+	require.NoError(t, err)
+	require.NotEmpty(t, hackathon2)
+
+	require.Equal(t, hackathon1.HackathonID, hackathon2.HackathonID)
+	require.Equal(t, hackathon1.Name, hackathon2.Name)
+	require.Equal(t, hackathon1.Icon, hackathon1.Icon)
+	require.Equal(t, hackathon1.Description, hackathon2.Description)
+	require.Equal(t, hackathon1.Link, hackathon2.Link)
+	require.Equal(t, hackathon1.Expired, hackathon2.Expired)
+	require.Equal(t, hackathon1.StartDate, hackathon2.StartDate)
+	require.Equal(t, hackathon1.Term, hackathon2.Term)
+}
+func TestListHackathon(t *testing.T) {
+	n := 5
+
+	for i := 0; i < n; i++ {
+		createHackathonTest(t)
+	}
+	arg := ListHackathonsParams{
+		Expired: time.Now(),
+		Limit:   int32(n),
+		Offset:  5,
+	}
+
+	hackathons, err := testQueries.ListHackathons(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, hackathons)
+	require.Len(t, hackathons, n)
+
+	for _, hackathon := range hackathons {
+		require.NotEmpty(t, hackathon)
+	}
 }
