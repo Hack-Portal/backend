1d725cf4840241614334b9b87bf8703e6d650c04
murasame <oogiriminister@gmail.com>
2023-07-11 21:15:04 +0900

    add:bookmark frame

---
+++ api/bookmark.go
@@ -0,0 +1,18 @@
+package api
+
+import "github.com/gin-gonic/gin"
+
+// ブックマークを作る
+func (server *Server) CreateBookmark(ctx *gin.Context) {
+
+}
+
+// ブックマークを削除する
+func (server *Server) RemoveBookmark(ctx *gin.Context) {
+
+}
+
+// ブックマーク一覧からハッカソンを取得する
+func (server *Server) ListBookmarkToHackathon(ctx *gin.Context) {
+
+}
