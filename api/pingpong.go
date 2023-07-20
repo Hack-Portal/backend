9770a8eb19423b1d96a3d4e69f28c10ac2581fc1
murasame <oogiriminister@gmail.com>
2023-07-21 01:37:32 +0900

    fix :ping

--- api/pingpong.go
+++ api/pingpong.go
@@ -1,8 +1,6 @@
 package api
 
 import (
-	"bytes"
-	"io"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,36 +8,7 @@ import (
 )
 
 func (server *Server) Ping(ctx *gin.Context) {
-	var imageURL string
-	file, _, err := ctx.Request.FormFile(ImageKey)
-	if err != nil {
-		switch err.Error() {
-		case MultiPartNextPartEoF:
-			ctx.JSON(400, errorResponse(err))
-			return
-		case HttpNoSuchFile:
-			ctx.JSON(400, errorResponse(err))
-			return
-		default:
-			ctx.JSON(400, errorResponse(err))
-			return
-		case RequestContentTypeIsnt:
-			break
-		}
-	} else {
-		buf := bytes.NewBuffer(nil)
-		if _, err := io.Copy(buf, file); err != nil {
-			ctx.JSON(500, errorResponse(err))
-			return
-		}
-		imageURL, err = server.store.UploadImage(ctx, buf.Bytes())
-		if _, err := io.Copy(buf, file); err != nil {
-			ctx.JSON(500, errorResponse(err))
-			return
-		}
-	}
-
-	ctx.JSON(http.StatusOK, gin.H{"message": imageURL})
+	ctx.JSON(http.StatusOK, gin.H{"message": "pong"})
 }
 
 func (server *Server) Pong(ctx *gin.Context) {
