58db3ae1bc1bafef09d13cd8bc821a97d538c252
murasame <oogiriminister@gmail.com>
2023-07-21 18:27:48 +0900

    add: tech_tagAPI swag

--- api/tech_tags.go
+++ api/tech_tags.go
@@ -6,6 +6,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// ListTechTags		godoc
+// @Summary			Get Framewroks
+// @Description		Get Framewroks
+// @Tags			TechTags
+// @Produce			json
+// @Success			200			{array}			db.TechTags	"succsss response"
+// @Failure 		500			{object}		ErrorResponse		"error response"
+// @Router       	/tech_tags	[get]
 func (server *Server) ListTechTags(ctx *gin.Context) {
 	techTags, err := server.store.ListTechTag(ctx)
 	if err != nil {
