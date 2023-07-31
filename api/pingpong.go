4de29e0a70e48b4ec4ff4bec549c2c3a395375c1
murasame <oogiriminister@gmail.com>
2023-07-31 18:25:45 +0900

    add: service and fix:ping

--- api/pingpong.go
+++ api/pingpong.go
@@ -1,12 +1,11 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func (server *Server) Ping(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{"message": pong})
+	ctx.JSON(http.StatusOK, gin.H{"message": "pong"})
 }
