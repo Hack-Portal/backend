4d8e831a074f9f551e3d013a378dcdcbafb44c96
murasame <oogiriminister@gmail.com>
2023-08-19 12:58:55 +0900

    add: Account tx test

--- pkg/adapter/gateways/repository/transaction/tx_account_test.go
+++ pkg/adapter/gateways/repository/transaction/tx_account_test.go
@@ -2,7 +2,6 @@ package transaction
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	repository "github.com/hackhack-Geek-vol6/backend/pkg/adapter/gateways/repository/datasource"
@@ -12,44 +11,103 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccountTx(t *testing.T) {
-	testCases := []struct {
-		name          string
-		arg           domain.CreateAccountParams
-		checkReturens func(t *testing.T, arg domain.CreateAccountParams, returns repository.Account)
-	}{
-		{
-			name: "success",
-			arg: domain.CreateAccountParams{
-				AccountInfo: repository.CreateAccountsParams{
-					AccountID:       util.RandomString(10),
-					Email:           util.RandomEmail(),
-					Username:        util.RandomString(10),
-					Icon:            dbutil.ToSqlNullString(util.RandomString(10)),
-					ExplanatoryText: dbutil.ToSqlNullString(util.RandomString(10)),
-					LocateID:        int32(util.Random(47)),
-					Rate:            0,
-					Character:       dbutil.ToSqlNullInt32(10),
-					ShowLocate:      false,
-					ShowRate:        false,
-				},
-				AccountTechTag:      util.RandomSelection(14, 5),
-				AccountFrameworkTag: util.RandomSelection(52, 5),
-			},
-			checkReturens: func(t *testing.T, arg domain.CreateAccountParams, returns repository.Account) {
-
-			},
+func randomAccount(t *testing.T, store *SQLStore) (domain.CreateAccountParams, repository.Account) {
+	arg := domain.CreateAccountParams{
+		AccountInfo: repository.CreateAccountsParams{
+			AccountID:       util.RandomString(10),
+			Email:           util.RandomEmail(),
+			Username:        util.RandomString(10),
+			Icon:            dbutil.ToSqlNullString(util.RandomString(10)),
+			ExplanatoryText: dbutil.ToSqlNullString(util.RandomString(10)),
+			LocateID:        int32(util.Random(47)),
+			Rate:            0,
+			Character:       dbutil.ToSqlNullInt32(int32(util.Random(5) + 1)),
+			ShowLocate:      false,
+			ShowRate:        false,
 		},
+		AccountTechTag:      util.RandomSelection(14, 5),
+		AccountFrameworkTag: util.RandomSelection(52, 5),
 	}
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			account, err := TestStore.CreateAccountTx(context.Background(), tc.arg)
-			require.NoError(t, err)
-			require.NotEmpty(t, account)
 
-			fmt.Println(account)
+	account, err := store.CreateAccountTx(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+	return arg, account
+}
+func TestCreateAccountTx(t *testing.T) {
+	store := NewStore(testDB, App)
 
-			tc.checkReturens(t, tc.arg, account)
-		})
+	arg, account := randomAccount(t, store)
+
+	tags, err := store.ListAccountTagsByUserID(context.Background(), account.AccountID)
+	require.NoError(t, err)
+
+	frameworks, err := store.ListAccountFrameworksByUserID(context.Background(), account.AccountID)
+	require.NoError(t, err)
+
+	require.Equal(t, arg.AccountInfo.AccountID, account.AccountID)
+	require.Equal(t, arg.AccountInfo.Username, account.Username)
+	require.Equal(t, arg.AccountInfo.Icon, account.Icon)
+	require.Equal(t, arg.AccountInfo.ExplanatoryText, account.ExplanatoryText)
+	require.Equal(t, arg.AccountInfo.LocateID, account.LocateID)
+	require.Equal(t, arg.AccountInfo.Rate, account.Rate)
+	require.Equal(t, arg.AccountInfo.Character, account.Character)
+	require.Equal(t, arg.AccountInfo.ShowLocate, account.ShowLocate)
+	require.Equal(t, arg.AccountInfo.ShowRate, account.ShowRate)
+	require.Equal(t, arg.AccountInfo.ShowLocate, account.ShowLocate)
+	require.NotEmpty(t, account.CreateAt)
+	require.NotEmpty(t, account.UpdateAt)
+	require.Equal(t, false, account.IsDelete)
+
+	require.Len(t, tags, len(arg.AccountTechTag))
+	require.Len(t, frameworks, len(arg.AccountFrameworkTag))
+}
+
+func TestUpdateAccountTx(t *testing.T) {
+	store := NewStore(testDB, App)
+	_, account := randomAccount(t, store)
+
+	arg := domain.UpdateAccountParam{
+		AccountInfo: repository.Account{
+			AccountID:       account.AccountID,
+			Email:           account.Email,
+			Username:        util.RandomString(10),
+			Icon:            dbutil.ToSqlNullString(util.RandomString(10)),
+			ExplanatoryText: dbutil.ToSqlNullString(util.RandomString(10)),
+			LocateID:        int32(util.Random(47)),
+			Rate:            account.Rate,
+			Character:       dbutil.ToSqlNullInt32(int32(util.Random(5) + 1)),
+			ShowLocate:      false,
+			ShowRate:        false,
+		},
+		AccountTechTag:      util.RandomSelection(14, 5),
+		AccountFrameworkTag: util.RandomSelection(52, 5),
 	}
+
+	account, err := store.UpdateAccountTx(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	tags, err := store.ListAccountTagsByUserID(context.Background(), account.AccountID)
+	require.NoError(t, err)
+
+	frameworks, err := store.ListAccountFrameworksByUserID(context.Background(), account.AccountID)
+	require.NoError(t, err)
+
+	require.Equal(t, arg.AccountInfo.AccountID, account.AccountID)
+	require.Equal(t, arg.AccountInfo.Username, account.Username)
+	require.Equal(t, arg.AccountInfo.Icon, account.Icon)
+	require.Equal(t, arg.AccountInfo.ExplanatoryText, account.ExplanatoryText)
+	require.Equal(t, arg.AccountInfo.LocateID, account.LocateID)
+	require.Equal(t, arg.AccountInfo.Rate, account.Rate)
+	require.Equal(t, arg.AccountInfo.Character, account.Character)
+	require.Equal(t, arg.AccountInfo.ShowLocate, account.ShowLocate)
+	require.Equal(t, arg.AccountInfo.ShowRate, account.ShowRate)
+	require.Equal(t, arg.AccountInfo.ShowLocate, account.ShowLocate)
+	require.NotEmpty(t, account.CreateAt)
+	require.NotEmpty(t, account.UpdateAt)
+	require.Equal(t, false, account.IsDelete)
+
+	require.Len(t, tags, len(arg.AccountTechTag))
+	require.Len(t, frameworks, len(arg.AccountFrameworkTag))
 }
