265e9f0674339a584fb477701c62fe50babb308c
murasame <oogiriminister@gmail.com>
2023-08-19 12:59:35 +0900

    add: rate entity test

---
+++ pkg/adapter/gateways/repository/transaction/tx_rate_entity_test.go
@@ -0,0 +1,28 @@
+package transaction
+
+import (
+	"context"
+	"testing"
+
+	repository "github.com/hackhack-Geek-vol6/backend/pkg/adapter/gateways/repository/datasource"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateRateEntityTx(t *testing.T) {
+	store := NewStore(testDB, App)
+
+	_, account1 := randomAccount(t, store)
+
+	arg := repository.CreateRateEntitiesParams{
+		AccountID: account1.AccountID,
+		Rate:      10,
+	}
+
+	require.NoError(t, store.CreateRateEntityTx(context.Background(), arg))
+
+	account2, err := store.GetAccountsByID(context.Background(), account1.AccountID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account2.Rate-account1.Rate, arg.Rate)
+}
