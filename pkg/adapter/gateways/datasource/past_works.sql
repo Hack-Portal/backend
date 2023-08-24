3bd914ca5fe1774402695bdfafc6bf232fba3f28
sikigasa <km3t1114@yahoo.co.jp>
2023-08-24 14:57:07 +0900

    fix: past_works/sql fmt

--- pkg/adapter/gateways/datasource/past_works.sql
+++ pkg/adapter/gateways/datasource/past_works.sql
@@ -15,16 +15,17 @@ SELECT *,
     WHERE likes.opus = past_works.opus
   ) as like_count,
   CASE
-    WHEN $2 IS NOT NULL AND EXISTS (
+    WHEN $2 IS NOT NULL
+    AND EXISTS (
       SELECT 1
       FROM likes
-      WHERE likes.opus = $1 AND likes.account_id = $2
+      WHERE likes.opus = $1
+        AND likes.account_id = $2
     ) THEN TRUE
     ELSE FALSE
   END as is_liked
 FROM past_works
 WHERE opus = $1;
-
 -- name: ListPastWorks :many
 SELECT opus,
   name,
@@ -35,10 +36,12 @@ SELECT opus,
     WHERE likes.opus = past_works.opus
   ) as like_count,
   CASE
-    WHEN $2 IS NOT NULL AND EXISTS (
+    WHEN $2 IS NOT NULL
+    AND EXISTS (
       SELECT 1
       FROM likes
-      WHERE likes.opus = past_works.opus AND likes.account_id = $3
+      WHERE likes.opus = past_works.opus
+        AND likes.account_id = $3
     ) THEN TRUE
     ELSE FALSE
   END as is_liked
