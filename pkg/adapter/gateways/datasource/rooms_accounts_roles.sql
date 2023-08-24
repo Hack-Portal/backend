765bb4224ea3634126987c4600d4f31e7eb9aa63
sikigasa <km3t1114@yahoo.co.jp>
2023-08-25 00:41:00 +0900

    fix: name

--- pkg/adapter/gateways/datasource/rooms_accounts_roles.sql
+++ pkg/adapter/gateways/datasource/rooms_accounts_roles.sql
@@ -3,12 +3,12 @@ INSERT INTO rooms_accounts_roles (rooms_account_id, role_id)
 VALUES ($1, $2)
 RETURNING *;
 
--- name: ListRoomsAccountsRolesByRoomID :many
+-- name: ListRoomsAccountsRolesByID :many
 SELECT *
 FROM rooms_accounts_roles
 WHERE rooms_account_id = $1
 LIMIT $2 OFFSET $3;
 
--- name: DeleteRoomsAccountsRolesByRoomID :exec
+-- name: DeleteRoomsAccountsRolesByID :exec
 DELETE FROM rooms_accounts_roles
 WHERE rooms_account_id = $1;
