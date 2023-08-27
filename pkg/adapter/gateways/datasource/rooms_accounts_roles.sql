fc978cb90622e421fffb3584cf8ceeaac42e0f92
sikigasa <km3t1114@yahoo.co.jp>
2023-08-27 22:36:09 +0900

    fix:sql

--- pkg/adapter/gateways/datasource/rooms_accounts_roles.sql
+++ pkg/adapter/gateways/datasource/rooms_accounts_roles.sql
@@ -3,11 +3,13 @@ SELECT rooms_account_id
 FROM rooms_accounts
 WHERE room_id = $1
   AND account_id = $2;
+
 -- name: CreateRoomsAccountsRoles :one
 INSERT INTO rooms_accounts_roles (rooms_account_id, role_id)
 VALUES ($1, $2)
 RETURNING *;
--- name: ListRoomsAccountsRolesByID :many
+
+-- name: ListRoomsAccountsRolesByIDs :many
 SELECT roles.role_id,
   roles.role
 FROM roles
@@ -18,6 +20,14 @@ WHERE rooms_accounts_roles.rooms_account_id = (
     WHERE room_id = $1
       AND account_id = $2
   );
+
+-- name: ListRoomsAccountsRolesByID :many
+SELECT roles.role_id,
+  roles.role
+FROM roles
+  LEFT OUTER JOIN rooms_accounts_roles ON rooms_accounts_roles.role_id = roles.role_id
+WHERE rooms_accounts_roles.rooms_account_id = $1;
+
 -- name: DeleteRoomsAccountsRolesByID :exec
 DELETE FROM rooms_accounts_roles
 WHERE rooms_account_id = $1
