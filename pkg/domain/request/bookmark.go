1c628e5ec81bda6d61f724fde8f975cef948039b
murasame <oogiriminister@gmail.com>
2023-09-24 01:54:59 +0900

    rm :bookmark domain

--- pkg/domain/request/bookmark.go
+++
@@ -1,9 +0,0 @@
-package request
-
-type CreateBookmark struct {
-	AccountID string `json:"account_id"`
-	Opus      int32  `json:"opus"`
-}
-type RemoveBookmark struct {
-	Opus int32 `query:"opus" binding:"required"`
-}
