390c0bc03b19cfac4e076364b1a0999aac2d3a00
murasame29 <oogiriminister@gmail.com>
2023-10-23 23:08:44 +0900

    fix:main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"context"
+	"time"
 
 	firebase "firebase.google.com/go"
 	"github.com/hackhack-Geek-vol6/backend/cmd/config"
 	"github.com/hackhack-Geek-vol6/backend/pkg/logger"
 	"github.com/hackhack-Geek-vol6/backend/pkg/postgres"
-	"github.com/hackhack-Geek-vol6/backend/src/transaction"
 	_ "github.com/lib/pq"
 	"google.golang.org/api/option"
 )
@@ -27,14 +27,16 @@ import (
 // @host		https://api-test.seafood-dev.com
 // @BasePath	/v1
 func main() {
-	l := logger.NewLogger(logger.DEBUG)
-	config.LoadEnv(l)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(config.Config.Server.ContextTimeout))
+	defer cancel()
 
-	pool, err := postgres.NewConnection(l).Connection()
+	pcon := postgres.NewConnection(l)
+	defer pcon.Close(ctx)
+
+	db, err := pcon.Connection()
 	if err != nil {
 		l.Panicf("database connection error :%v", err)
 	}
-	defer pool.Close()
 
 	serviceAccount := option.WithCredentialsFile("./serviceAccount.json")
 	app, err := firebase.NewApp(context.Background(), &firebase.Config{
@@ -44,8 +46,13 @@ func main() {
 		l.Panicf("firebase connection error :%v", err)
 	}
 
-	store := transaction.NewStore(pool)
-
 	// 以下サーバー起動設定
 
+}
+
+var l logger.Logger
+
+func init() {
+	l = logger.NewLogger(logger.DEBUG)
+	config.LoadEnv(l)
 }
