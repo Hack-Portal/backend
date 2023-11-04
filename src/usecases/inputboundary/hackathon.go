725ecbc9a5d1518c3293ef6e62f824bdd2642125
murasame <oogiriminister@gmail.com>
2023-11-04 09:46:11 +0900

    fix: create usecaseの引数変更

--- src/usecases/inputboundary/hackathon.go
+++ src/usecases/inputboundary/hackathon.go
@@ -1,12 +1,15 @@
 package inputboundary
 
-import "github.com/hackhack-Geek-vol6/backend/src/datastructs/input"
+import (
+	"github.com/hackhack-Geek-vol6/backend/src/datastructs/input"
+	"github.com/hackhack-Geek-vol6/backend/src/datastructs/output"
+)
 
 // ここでは主にUsecase Interactorを汎化している
 
 // TODO:しっかりとしたインターフェースを作る　一旦は仮置き
 type HackathonInputPort interface {
-	Create(input.HackathonCreate, []byte)
+	Create(input.HackathonCreate, []byte) (int, *output.CreateHackathon)
 	Read()
 	Update()
 	Delete()
--- src/usecases/outputboundary/hackathon.go
+++ src/usecases/outputboundary/hackathon.go
@@ -1,10 +1,12 @@
 package outputboundary
 
+import "github.com/hackhack-Geek-vol6/backend/src/datastructs/output"
+
 // ここでは主にPresenterを汎化している
 
 // TODO:ここもどうようにするか考える
 type HackathonOutputPort interface {
-	Create(error)
+	Create(error) (int, *output.CreateHackathon)
 	Read()
 	Update()
 	Delete()
