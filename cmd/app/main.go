7beb2b1a58ff216b1893d7c35176b79bfed29b3e
murasame <oogiriminister@gmail.com>
2023-11-04 09:48:22 +0900

    add: main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"context"
+
+	"github.com/hackhack-Geek-vol6/backend/src/drivers/postgres"
+	"github.com/hackhack-Geek-vol6/backend/src/infrastructure/server"
+)
+
+func main() {
+	conn := postgres.NewConnection()
+	defer conn.Close(context.Background())
+
+	dbconn, err := conn.Connection()
+	if err != nil {
+		panic(err)
+	}
+
+	server.NewGinServer(dbconn).Run()
+
+}
