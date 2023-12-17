eedc70178af943b36d064bf16dc7fda4c4bae109
murasame <oogiriminister@gmail.com>
2023-12-18 01:19:32 +0900

    fix: statusTags entities pk field

--- src/datastructure/models/entities.go
+++ src/datastructure/models/entities.go
@@ -7,8 +7,8 @@ CREATE TABLE "status_tags" (
 );
 */
 type StatusTag struct {
-	// ID is primary key and auto increment
+	// StatusID is primary key and auto increment
 	// AutoIncrementなので、Createの際には指定する必要はない
-	ID     int64  `json:"id"`
-	Status string `json:"status"`
+	StatusID int64  `json:"status_id"`
+	Status   string `json:"status"`
 }
