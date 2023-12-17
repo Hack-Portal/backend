0308bb97d5b06c3dfa8796a0d872cdec42b9e548
murasame <oogiriminister@gmail.com>
2023-12-17 17:23:28 +0900

    add: gorm connection

--- go.mod
+++ go.mod
@@ -8,13 +8,21 @@ require (
 )
 
 require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/exp v0.0.0-20231214170342-aacd6d4b4611 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
+	gorm.io/driver/postgres v1.5.4 // indirect
+	gorm.io/gorm v1.25.5 // indirect
 )
---
+++ src/frameworks/db/gorm/gorm.go
@@ -0,0 +1,202 @@
+package gorm
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/hackhack-Geek-vol6/backend/src/frameworks/db"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Connection interface {
+	Connection() (*gorm.DB, error)
+	Close(ctx context.Context) error
+}
+
+type gormDatabaseConnection struct {
+	dsn string
+
+	connectAttempts        int
+	connectWaitTimeSeconds int
+	connectBlocks          bool
+	connecting             bool
+	connectError           error
+
+	status          db.Status
+	conn            *gorm.DB
+	connectFinished chan bool
+
+	logger *slog.Logger
+}
+
+func NewGormConnection(
+	dsn string,
+	connectAttempts int,
+	connectWaitTimeSeconds int,
+	connectBlocks bool,
+
+	logger *slog.Logger,
+) Connection {
+	if logger == nil {
+		logger = slog.Default().With(slog.String("package", "db"), slog.String("framework", "gorm"))
+	}
+
+	if connectAttempts == 0 {
+		connectAttempts = 1
+	}
+
+	if connectWaitTimeSeconds == 0 {
+		connectWaitTimeSeconds = 3
+	}
+
+	return &gormDatabaseConnection{
+		dsn:                    dsn,
+		connectAttempts:        connectAttempts,
+		connectWaitTimeSeconds: connectWaitTimeSeconds,
+		connectBlocks:          connectBlocks,
+		connectFinished:        make(chan bool, 1),
+		status:                 db.UNKNOWN,
+		logger:                 logger,
+	}
+}
+
+func (c *gormDatabaseConnection) Connection() (*gorm.DB, error) {
+	c.updateDBStatus()
+
+	if c.status != db.READY {
+		c.tryConnect()
+	}
+
+	if c.status == db.READY {
+		return c.conn, nil
+	}
+
+	return nil, fmt.Errorf("database connection is not ready: %v, %v", c.status, c.connectError)
+}
+
+// tryConnect attempts opening single connection to the database.
+func (c *gormDatabaseConnection) tryConnect() {
+	if !c.connecting {
+		if c.connectBlocks {
+			c.connect()
+		} else {
+			go c.connect()
+		}
+	} else if c.connectBlocks {
+		// different goroutine is connecting, wait until finished
+		<-c.connectFinished
+	}
+}
+
+func (c *gormDatabaseConnection) connect() {
+	c.connecting = true
+
+	gormConnect := func() (*gorm.DB, error) {
+		return gorm.Open(postgres.Open(c.dsn), &gorm.Config{})
+	}
+	sleep := func(seconds int) {
+		time.Sleep(time.Duration(seconds) * time.Second)
+	}
+
+	if c.connectAttempts < 0 {
+		for c.status != db.READY {
+			c.conn, c.connectError = gormConnect()
+			c.updateDBStatus()
+
+			if c.status != db.READY {
+				c.logger.Warn("unable to connect to database: %v. retrying after %d seconds", c.connectError, c.connectWaitTimeSeconds)
+				sleep(c.connectWaitTimeSeconds)
+			}
+		}
+
+		c.logger.Info("connected with gorm to postgres")
+	} else {
+		var err error
+		for i := 0; i < c.connectAttempts; i++ {
+			c.conn, err = gormConnect()
+			c.updateDBStatus()
+
+			if c.status != db.READY {
+				c.logger.Warn("unable to connect to database: %v", err)
+
+				if i < c.connectAttempts-1 {
+					sleep(c.connectWaitTimeSeconds)
+				}
+			} else {
+				c.logger.Info("connected with gorm to postgres")
+				break
+			}
+		}
+
+		if c.isConnNil() {
+			c.logger.Error("failed to connect to database in %d tries: %v", c.connectAttempts, err)
+			c.connectError = err
+		}
+	}
+
+	c.connecting = false
+	go func() {
+		c.connectFinished <- true
+	}()
+}
+
+func (c *gormDatabaseConnection) isConnNil() bool {
+	return db.IsInterfaceNil(c.conn)
+}
+
+func (c *gormDatabaseConnection) updateDBStatus() {
+	if c.isConnNil() {
+		c.status = db.NOT_READY
+		return
+	}
+
+	sqlDB, err := c.conn.DB()
+	if err != nil {
+		c.logger.Error("failed to get generic SQL database: %v", err)
+		c.status = db.ERROR
+
+		if err = c.Close(context.Background()); err == nil {
+			c.status = db.NOT_READY
+		}
+		return
+	}
+
+	if err := sqlDB.PingContext(context.Background()); err != nil {
+		c.logger.Error("failed to ping database: %v", err)
+		c.status = db.ERROR
+
+		if err = c.Close(context.Background()); err == nil {
+			c.status = db.NOT_READY
+		}
+	} else {
+		c.status = db.READY
+	}
+}
+
+// Close closes the connection to the database.
+func (c *gormDatabaseConnection) Close(ctx context.Context) error {
+	c.logger.Info("closing gorm postgres db connection")
+
+	if c.isConnNil() {
+		c.logger.Debug("no connection to close")
+		return nil
+	}
+
+	sqlDB, err := c.conn.DB()
+	if err != nil {
+		c.logger.Error("failed to get generic SQL database: %v", err)
+		c.status = db.ERROR
+	}
+
+	err = sqlDB.Close()
+	if err != nil {
+		c.logger.Error("failed to close connection: %v", err)
+	}
+
+	c.status = db.DISCONNECTED
+	c.conn = nil
+	return err
+}
