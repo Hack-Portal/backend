105794536c36dc85841a49cbd52a4b6693893e05
murasame <oogiriminister@gmail.com>
2023-12-22 15:09:26 +0900

    add: validate form

--- src/datastructure/request/hackathon.go
+++ src/datastructure/request/hackathon.go
@@ -3,9 +3,9 @@ package request
 import "time"
 
 type CreateHackathon struct {
-	Name      string    `json:"name"`
-	Link      string    `json:"link"`
-	Expired   time.Time `json:"expired"`
-	StartDate time.Time `json:"start_date"`
-	Term      int       `json:"term"`
+	Name      string    `json:"name" validate:"required"`
+	Link      string    `json:"link" validate:"required"`
+	Expired   time.Time `json:"expired" validate:"required"`
+	StartDate time.Time `json:"start_date" validate:"required"`
+	Term      int       `json:"term" validate:"required"`
 }
