702d269244ba3e736b78ffe9e5efb9fef6ce4f3d
murasame <oogiriminister@gmail.com>
2023-12-28 15:41:36 +0900

    add:new relic middleware

--- src/frameworks/echo/echo.go
+++ src/frameworks/echo/echo.go
@@ -6,6 +6,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/labstack/echo/v4"
 	echoMiddleware "github.com/labstack/echo/v4/middleware"
+	"github.com/newrelic/go-agent/v3/integrations/nrecho-v4"
+	"github.com/newrelic/go-agent/v3/newrelic"
 	"gorm.io/gorm"
 )
 
@@ -13,14 +15,16 @@ type echoServer struct {
 	engine *echo.Echo
 	v1     *echo.Group
 
+	app    *newrelic.Application
 	db     *gorm.DB
 	client *s3.Client
 	logger *slog.Logger
 }
 
-func NewEchoServer(db *gorm.DB, client *s3.Client, logger *slog.Logger) *echo.Echo {
+func NewEchoServer(db *gorm.DB, client *s3.Client, app *newrelic.Application, logger *slog.Logger) *echo.Echo {
 	router := &echoServer{
 		engine: echo.New(),
+		app:    app,
 		client: client,
 		db:     db,
 	}
@@ -42,30 +46,14 @@ func NewEchoServer(db *gorm.DB, client *s3.Client, logger *slog.Logger) *echo.Ec
 }
 
 func (es *echoServer) setupMiddleware() {
+	// New Relic Setting
+	es.engine.Use(nrecho.Middleware(es.app))
+	// Cross-Origin Resource Sharing
 	es.engine.Use(
 		echoMiddleware.CORSWithConfig(echoMiddleware.CORSConfig{
 			AllowOrigins: []string{"*"},
 			AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType},
 			AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
 		}),
-	)
-	es.engine.Use(
-		echoMiddleware.RequestLoggerWithConfig(
-			echoMiddleware.RequestLoggerConfig{
-				LogStatus:    true,
-				LogURI:       true,
-				LogLatency:   true,
-				LogProtocol:  true,
-				LogRemoteIP:  true,
-				LogMethod:    true,
-				LogURIPath:   true,
-				LogRoutePath: true,
-				LogUserAgent: true,
-				LogError:     true,
-				LogValuesFunc: func(c echo.Context, v echoMiddleware.RequestLoggerValues) error {
-					return nil
-				},
-			},
-		),
 	)
 }
