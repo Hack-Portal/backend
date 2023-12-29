447f42a5a834882a54573d7fd286eb9395ab2290
murasame <oogiriminister@gmail.com>
2023-12-30 08:04:28 +0900

    Add Redis cache client to CloudflareR2 gateway

--- src/adapters/gateways/cloudflare_r2.go
+++ src/adapters/gateways/cloudflare_r2.go
@@ -11,6 +11,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/newrelic/go-agent/v3/newrelic"
+	"github.com/redis/go-redis/v9"
 )
 
 type CloudflareR2 struct {
@@ -18,16 +19,19 @@ type CloudflareR2 struct {
 	PresignClient *s3.PresignClient
 	bucket        string
 	Config        Config
+
+	cacheClient dai.Cache[string]
 }
 
 type Config struct {
 	PresignLinkExpired time.Duration
 }
 
-func NewCloudflareR2(bucket string, client *s3.Client, presignLinkExpired int) dai.FileStore {
+func NewCloudflareR2(bucket string, client *s3.Client, cache *redis.Client, presignLinkExpired int) dai.FileStore {
 	return &CloudflareR2{
 		bucket:        bucket,
 		client:        client,
+		cacheClient:   NewCache[string](cache, time.Duration(30)*time.Minute),
 		PresignClient: s3.NewPresignClient(client),
 		Config: Config{
 			PresignLinkExpired: time.Duration(presignLinkExpired),
@@ -50,17 +54,19 @@ func (c *CloudflareR2) ListObjects(ctx context.Context, key string) (*s3.ListObj
 
 func (c *CloudflareR2) GetPresignedObjectURL(ctx context.Context, key string) (string, error) {
 	defer newrelic.FromContext(ctx).StartSegment("GetPresignedObjectURL-gateway").End()
-
-	object, err := c.PresignClient.PresignGetObject(ctx, &s3.GetObjectInput{
-		Bucket:          aws.String(c.bucket),
-		Key:             aws.String(key),
-		ResponseExpires: aws.Time(time.Now().Add(c.Config.PresignLinkExpired * time.Hour)),
+	url, err := c.cacheClient.Get(ctx, key, func(ctx context.Context) (string, error) {
+		object, err := c.PresignClient.PresignGetObject(ctx, &s3.GetObjectInput{
+			Bucket:          aws.String(c.bucket),
+			Key:             aws.String(key),
+			ResponseExpires: aws.Time(time.Now().Add(c.Config.PresignLinkExpired * time.Hour)),
+		})
+		if err != nil {
+			return "", err
+		}
+		return object.URL, nil
 	})
-	if err != nil {
-		return "", err
-	}
 
-	return object.URL, nil
+	return url, err
 }
 
 func (c *CloudflareR2) UploadFile(ctx context.Context, file []byte, key string) (string, error) {
