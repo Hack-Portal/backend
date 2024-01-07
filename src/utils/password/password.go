5b6fa2c841d34d38a3b48b23694d17e6ebfec16d
murasame <oogiriminister@gmail.com>
2024-01-07 23:11:38 +0900

    add: easy password hash util

---
+++ src/utils/password/password.go
@@ -0,0 +1,21 @@
+package password
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashedPasswordはbcryptによってハッシュ化されたパスワードをかえす
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("failed to hash password: %w", err)
+	}
+	return string(hashedPassword), nil
+}
+
+// bcryptによってハッシュ化されたパスワードを検証する
+func CheckPassword(password string, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
