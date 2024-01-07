77668c7c217c5436a7b79d1b79be94283e1f9a6c
murasame <oogiriminister@gmail.com>
2024-01-08 01:09:24 +0900

    一旦このまま

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"log/slog"
-	"os"
-	"time"
+	"log"
 
 	"github.com/Hack-Portal/backend/cmd/config"
 	"github.com/Hack-Portal/backend/cmd/migrations"
-	"github.com/Hack-Portal/backend/src/driver/aws"
-	"github.com/Hack-Portal/backend/src/driver/newrelic"
-	"github.com/Hack-Portal/backend/src/driver/redis"
-	"github.com/Hack-Portal/backend/src/frameworks/db/gorm"
-	"github.com/Hack-Portal/backend/src/frameworks/echo"
+	"github.com/Hack-Portal/backend/src/router"
 	"github.com/Hack-Portal/backend/src/server"
-	"github.com/murasame29/db-conn/sqldb/postgres"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func init() {
@@ -37,88 +31,64 @@ func init() {
 // @host							api-dev.hack-portal.com
 // @BasePath					/v1
 func main() {
-	logger := slog.New(slog.NewJSONHandler(
-		os.Stdout,
-		&slog.HandlerOptions{
-			AddSource: true,
-			Level:     slog.LevelDebug,
-		},
-	))
-
-	postgresConn := postgres.NewConnection(
+	dsn := fmt.Sprintf("user=%s password=%s dbname=%s port=%d sslmode=%s TimeZone=%s",
 		config.Config.Database.User,
 		config.Config.Database.Password,
-		config.Config.Database.Host,
-		config.Config.Database.Port,
 		config.Config.Database.DBName,
+		config.Config.Database.Port,
 		config.Config.Database.SSLMode,
-		config.Config.Database.ConnectAttempts,
-		config.Config.Database.ConnectWaitTime,
-		config.Config.Database.ConnectBlocks,
+		config.Config.Database.TimeZone,
 	)
 
-	sqlDB, err := postgresConn.Connection()
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true,
+	}))
 	if err != nil {
-		logger.Error(fmt.Sprintf("failed to connect database: %v", err))
-		return
+		log.Fatal(err)
 	}
 
-	// open db connection
-	gorm := gorm.New()
-	dbconn, err := gorm.Connection(sqlDB)
-	if err != nil {
-		logger.Error(fmt.Sprintf("failed to connect database: %v", err))
-		return
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	defer gorm.Close(ctx)
+	sqlDB, _ := db.DB()
 
 	// migrate
 	m, err := migrations.NewPostgresMigrate(sqlDB, "file://cmd/migrations", nil)
 	if err != nil {
-		logger.Error(fmt.Sprintf("failed create migrate instance: %v", err))
 		return
 	}
 	// migrate up
 	m.Up()
 
-	client, err := aws.New(
-		config.Config.Buckets.AccountID,
-		config.Config.Buckets.EndPoint,
-		config.Config.Buckets.AccessKeyId,
-		config.Config.Buckets.AccessKeySecret,
-	).Connect(context.Background())
+	// client, err := aws.New(
+	// 	config.Config.Buckets.AccountID,
+	// 	config.Config.Buckets.EndPoint,
+	// 	config.Config.Buckets.AccessKeyId,
+	// 	config.Config.Buckets.AccessKeySecret,
+	// ).Connect(context.Background())
 
-	app, err := newrelic.Setup()
-	if err != nil {
-		logger.Error(fmt.Sprintf("failed to setup newrelic: %v", err))
-		return
-	}
+	// app, err := newrelic.Setup()
+	// if err != nil {
+	// 	logger.Error(fmt.Sprintf("failed to setup newrelic: %v", err))
+	// 	return
+	// }
 
-	redisconn := redis.New(
-		fmt.Sprintf("%v:%v", config.Config.Redis.Host, config.Config.Redis.Port),
-		config.Config.Redis.Password,
-		&config.Config.Redis.ConnectTimeout,
-		&config.Config.Redis.ConnectAttempts,
-		&config.Config.Redis.ConnectWaitTime,
-	)
-	defer redisconn.Close()
+	// redisconn := redis.New(
+	// 	fmt.Sprintf("%v:%v", config.Config.Redis.Host, config.Config.Redis.Port),
+	// 	config.Config.Redis.Password,
+	// 	&config.Config.Redis.ConnectTimeout,
+	// 	&config.Config.Redis.ConnectAttempts,
+	// 	&config.Config.Redis.ConnectWaitTime,
+	// )
+	// defer redisconn.Close()
 
-	redisConn, err := redisconn.Connect(config.Config.Redis.DB)
-	if err != nil {
-		logger.Error(fmt.Sprintf("failed to connect redis: %v", err))
-		return
-	}
+	// redisConn, err := redisconn.Connect(config.Config.Redis.DB)
+	// if err != nil {
+	// 	logger.Error(fmt.Sprintf("failed to connect redis: %v", err))
+	// 	return
+	// }
 
 	// start server
-	handler := echo.NewEchoServer(
-		dbconn,
-		client,
-		redisConn,
-		app,
-		logger,
+	handler := router.NewRouter(
+		*router.NewDebug(config.Config.Server.Version),
 	)
 
 	server.New().Run(handler)
