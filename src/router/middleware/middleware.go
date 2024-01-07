c00ee765279b0026cb903618f023b5ff5295c6d3
murasame <oogiriminister@gmail.com>
2024-01-08 01:16:30 +0900

    Refactor middleware package to use new user gateway

--- src/router/middleware/middleware.go
+++ src/router/middleware/middleware.go
@@ -1,7 +1,17 @@
 package middleware
 
-import "github.com/Hack-Portal/backend/src/router/middleware/auth"
+import (
+	"github.com/Hack-Portal/backend/src/adapters/gateways"
+	"github.com/Hack-Portal/backend/src/router/middleware/auth"
+	"gorm.io/gorm"
+)
 
-type Middleware interface {
+type middleware struct {
 	auth.Auth
+}
+
+func NewMiddleware(db *gorm.DB) *middleware {
+	return &middleware{
+		Auth: auth.NewBasicAuth(gateways.NewUserGateway(db)),
+	}
 }
