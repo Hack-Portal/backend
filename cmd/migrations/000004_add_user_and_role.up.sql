a4b6cc9a437c15a87223f44be15969c245a95eb9
murasame <oogiriminister@gmail.com>
2024-01-08 01:02:23 +0900

    fix:migrate

--- cmd/migrations/000004_add_user_and_role.up.sql
+++ cmd/migrations/000004_add_user_and_role.up.sql
@@ -1,19 +1,21 @@
-CREATE TABLE "applove_user" (
-  "hackathon_id" varchar NOT NULL,
-  "user_id" varchar NOT NULL
-);
+
 
 CREATE TABLE "users" (
   "user_id" varchar PRIMARY KEY,
   "name" varchar NOT NULL,
-  "password" varchar NOT NULL,
-  "role" varchar NOT NULL
+  "password" text NOT NULL,
+  "role" int NOT NULL
 );
 
 
 CREATE TABLE "roles" (
   "role_id" serial PRIMARY KEY,
   "role" varchar NOT NULL
+);
+
+CREATE TABLE "applove_user" (
+  "hackathon_id" varchar NOT NULL,
+  "user_id" varchar NOT NULL
 );
 
 CREATE INDEX ON "applove_user" ("hackathon_id");
@@ -28,6 +30,6 @@ CREATE INDEX ON "roles" ("role_id");
 
 ALTER TABLE "users" ADD FOREIGN KEY ("role") REFERENCES "roles" ("role_id");
 
-ALTER TABLE "users" ADD FOREIGN KEY ("user_id") REFERENCES "applove_user" ("user_id");
-
 ALTER TABLE "applove_user" ADD FOREIGN KEY ("hackathon_id") REFERENCES "hackathons" ("hackathon_id");
+
+ALTER TABLE "applove_user" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("user_id");
---
+++ cmd/migrations/000005_init_role.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO roles (role) VALUES ('admin');
+INSERT INTO roles (role) VALUES ('hack-portal-operator');
+INSERT INTO roles (role) VALUES ('guest');
