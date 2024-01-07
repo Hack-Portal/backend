13297a114f479a5b2a8eb5e65df3b449b2c8594a
murasame <oogiriminister@gmail.com>
2024-01-07 23:11:09 +0900

    add : new migrate

---
+++ cmd/migrations/000004_add_user_and_role.up.sql
@@ -0,0 +1,33 @@
+CREATE TABLE "applove_user" (
+  "hackathon_id" varchar NOT NULL,
+  "user_id" varchar NOT NULL
+);
+
+CREATE TABLE "users" (
+  "user_id" varchar PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "password" varchar NOT NULL,
+  "role" varchar NOT NULL
+);
+
+
+CREATE TABLE "roles" (
+  "role_id" serial PRIMARY KEY,
+  "role" varchar NOT NULL
+);
+
+CREATE INDEX ON "applove_user" ("hackathon_id");
+
+CREATE INDEX ON "applove_user" ("user_id");
+
+CREATE INDEX ON "users" ("user_id");
+
+CREATE INDEX ON "users" ("role");
+
+CREATE INDEX ON "roles" ("role_id");
+
+ALTER TABLE "users" ADD FOREIGN KEY ("role") REFERENCES "roles" ("role_id");
+
+ALTER TABLE "users" ADD FOREIGN KEY ("user_id") REFERENCES "applove_user" ("user_id");
+
+ALTER TABLE "applove_user" ADD FOREIGN KEY ("hackathon_id") REFERENCES "hackathons" ("hackathon_id");
