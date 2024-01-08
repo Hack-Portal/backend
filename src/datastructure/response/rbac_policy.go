15e54afe7484ff53de52e75735b39b27dc1c9089
murasame <oogiriminister@gmail.com>
2024-01-08 21:15:53 +0900

    リクエストとレスポンスのデータ型を追加

---
+++ src/datastructure/response/rbac_policy.go
@@ -0,0 +1,18 @@
+package response
+
+import "github.com/Hack-Portal/backend/src/datastructure/models"
+
+type CreateRbacPolicy struct {
+	Id []int `json:"id"`
+}
+
+type ListRbacPolicies struct {
+	Policies []*models.RbacPolicy `json:"policies"`
+}
+
+type DeleteRbacPolicy struct {
+	PolicyID int64 `json:"policy_id"`
+}
+
+type DeleteAllRbacPolicies struct {
+}
