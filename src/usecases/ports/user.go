100ec4a0ad67dc3ddc071c5264aacf5393f6b12f
murasame <oogiriminister@gmail.com>
2024-01-10 17:32:19 +0900

    add: usecase interfaces

--- src/usecases/ports/user.go
+++ src/usecases/ports/user.go
@@ -9,10 +9,12 @@ import (
 
 type UserInputBoundary interface {
 	InitAdmin(ctx context.Context, in request.InitAdmin) (int, *response.User)
+	Login(ctx context.Context, in request.Login) (int, *response.Login)
 }
 
 type UserOutputBoundary interface {
 	PresentInitAdmin(ctx context.Context, out *OutputInitAdminData) (int, *response.User)
+	PresentLogin(ctx context.Context, out OutputBoundary[*response.Login]) (int, *response.Login)
 }
 
 type OutputInitAdminData struct {
