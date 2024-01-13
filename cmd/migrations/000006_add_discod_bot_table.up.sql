104e5095c86da7ae85bf96565f5db53297cc199d
murasame29 <oogiriminister@gmail.com>
2024-01-13 11:54:08 +0900

    fix: discord channel master

--- cmd/migrations/000006_add_discod_bot_table.up.sql
+++ cmd/migrations/000006_add_discod_bot_table.up.sql
@@ -4,16 +4,15 @@ CREATE TABLE "hackathon_discord_channels" (
 );
 
 CREATE TABLE "discord_server_registries" (
-  "guild_id" varchar PRIMARY KEY,
-  "server_name" varchar NOT NULL,
-  "selected_channel" varchar NOT NULL
+  "channel_id" varchar PRIMARY KEY,
+  "channel_name" varchar NOT NULL
 );
 
 CREATE TABLE "discord_server_forum_tags" (
-  "guild_id" varchar NOT NULL,
-  "status_id" varchar NOT NULL,
+  "channel_id" varchar NOT NULL,
+  "status_id" int NOT NULL,
   "forum_id" varchar NOT NULL
 );
 
 ALTER TABLE "hackathon_discord_channels" ADD FOREIGN KEY ("hackathon_id") REFERENCES "hackathons" ("hackathon_id");
-ALTER TABLE "discord_server_forum_tags" ADD FOREIGN KEY ("status_id") REFERENCES "status_tags" ("status_id");
+ALTER TABLE "discord_server_forum_tags" ADD FOREIGN KEY ("channel_id") REFERENCES "discord_server_registries" ("channel_id");
