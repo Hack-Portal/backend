89317fb38bf98fb5e93740b36393d14d4901e943
sikigasa <km3t1114@yahoo.co.jp>
2024-03-23 01:15:51 +0900

    fix: sql

--- cmd/migrations/000006_add_hackathon_proposals.up.sql
+++ cmd/migrations/000006_add_hackathon_proposals.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE "hackathon_proposals" (
   "hackathon_proposal_id" serial PRIMARY KEY,
   "url" varchar NOT NULL,
   "is_approved" BOOLEAN NOT NULL DEFAULT FALSE,
-  "created_at" timestamptz NOT NULL,
+  "created_at" timestamptz NOT NULL
 );
