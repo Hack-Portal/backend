8c91792326d05d6b09724dedb7c07e8c21dc43a6
sikigasa <km3t1114@yahoo.co.jp>
2024-03-22 17:12:03 +0900

    add: req&res

--- src/datastructure/request/hackathon_proposal.go
+++ src/datastructure/request/hackathon_proposal.go
@@ -3,3 +3,22 @@ package request
 type CreateHackathonProposal struct {
 	URL string `json:"url"`
 }
+
+type ListHackathonProposal struct {
+	PageSize int `query:"page_size"`
+	PageID   int `query:"page_id"`
+}
+
+type GetHackathonProposal struct {
+	HackathonProposalID int `json:"hackathon_proposal_id"`
+}
+
+type UpdateHackathonProposal struct {
+	HackathonProposalID int    `json:"hackathon_proposal_id"`
+	URL                 string `json:"url"`
+	IsApproved          bool   `json:"isApproved"`
+}
+
+type DeleteHackathonProposal struct {
+	HackathonProposalID int `json:"hackathon_proposal_id"`
+}
--- src/datastructure/response/hackathon_proposal.go
+++ src/datastructure/response/hackathon_proposal.go
@@ -8,3 +8,21 @@ type CreateHackathonProposal struct {
 	IsApproved          bool      `json:"is_approved"`
 	CreatedAt           time.Time `json:"created_at"`
 }
+
+type GetHackathonProposal struct {
+	HackathonProposalID int       `json:"hackathon_proposal_id"`
+	URL                 string    `json:"url"`
+	IsApproved          bool      `json:"is_approved"`
+	CreatedAt           time.Time `json:"created_at"`
+}
+
+type UpdateHackathonProposal struct {
+	HackathonProposalID int       `json:"hackathon_proposal_id"`
+	URL                 string    `json:"url"`
+	IsApproved          bool      `json:"is_approved"`
+	CreatedAt           time.Time `json:"created_at"`
+}
+
+type DeleteHackathonProposal struct {
+	HackathonProposalID int `json:"hackathon_proposal_id"`
+}
